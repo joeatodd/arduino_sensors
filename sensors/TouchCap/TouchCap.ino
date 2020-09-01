e7aadd0836b1180729354cbf0f542b4e099cfe3a
J Todd <jat39@st-andrews.ac.uk>
2020-09-01 12:20:15 +0100

    Finish off TouchCap.ino for bedroom light switch

--- sensors/TouchCap/TouchCap.ino
+++ sensors/TouchCap/TouchCap.ino
@@ -1,13 +1,44 @@
+// ==== Radio Stuff ====
+// Enable debug prints
+#define MY_DEBUG
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+//#define MY_RS485
+
+#define MY_NODE_ID 4
+
+#define CHILD_ID_BUTT 0
+
+#include <SPI.h>
+#include <MySensors.h>
+
+MyMessage msgButt(CHILD_ID_BUTT, V_STATUS);
+
+// ==== Cap Stuff ====
+
 #include <CapacitiveSensor.h>
 CapacitiveSensor Sensor = CapacitiveSensor(4, 2);
 long val;
 int pos;
-#define led 13
+
+
+// Radio stuff
+void presentation()
+{
+  // Send the sketch version information to the gateway
+  sendSketchInfo("TouchCap", "1.0");
+
+  // Register all sensors to gw (they will be created as child devices)
+  present(CHILD_ID_BUTT, S_BINARY);
+}
+
 
 void setup()
 {
 Serial.begin(9600);
-pinMode(led, OUTPUT);
+pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
@@ -17,14 +48,16 @@ val = Sensor.capacitiveSensor(30);
 Serial.println(val);
 if (val >= 1000 && pos == 0)
 {
-digitalWrite(led, HIGH);
+send(msgButt.set(1));
+digitalWrite(LED_BUILTIN, HIGH);
 pos = 1;
 delay(500);
 }
 
 else if (val >= 1000 && pos == 1)
 {
-digitalWrite(led, LOW);
+send(msgButt.set(0));
+digitalWrite(LED_BUILTIN, LOW);
 pos = 0;
 delay(500);
 }
