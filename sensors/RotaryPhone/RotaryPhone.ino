5ebb3d0111899c2e91c8602f0d0f2bd57ee3cb1e
J Todd <jat39@st-andrews.ac.uk>
2021-02-03 15:27:32 +0000

    Adding button to RotaryPhone

--- sensors/RotaryPhone/RotaryPhone.ino
+++ sensors/RotaryPhone/RotaryPhone.ino
@@ -1,21 +1,33 @@
+
+//#define RADIO
 #define INO_DEBUG
-#define MY_RADIO_NRF24
+
+#ifdef RADIO
+#define MY_DEBUG
+#define MY_RADIO_RF24
 
 #define MY_NODE_ID 5
 
 #define CHILD_ID_KEYS 0
 
 #include <SPI.h>
-#include <Keypad.h>
 #include <MySensors.h>
+#endif
+
+#include <Keypad.h>
+
+#define BUTTON_PIN A2
 
 // 4x4 keypad matrix (one whole row is just the central button)
 const byte rows = 4;
 const byte cols = 4;
 
-char test_msg[3] = "012";
+char test_msg[3] = "01";
 
+
+#ifdef RADIO
 MyMessage msgButt(CHILD_ID_KEYS, V_TEXT);
+#endif
 
 char keys[rows][cols] = {{'*','7','4','1'},
 			 {'0','8','5','2'},
@@ -35,11 +47,12 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
 void presentation(){
   /* Send the sketch version information to the gateway */
-  sendSketchInfo("RotaryPhone", "1.0");
 
+  #ifdef RADIO
+  sendSketchInfo("RotaryPhone", "1.0");
   /* Register all sensors to gw (they will be created as child devices) */
   present(CHILD_ID_KEYS, S_INFO);
-
+  #endif
 }
 
 void setup(){
@@ -48,13 +61,20 @@ void setup(){
   Serial.begin(9600);
 #endif
 
+// Setup the button
+pinMode(BUTTON_PIN, INPUT);
+// Activate internal pull-up
+digitalWrite(BUTTON_PIN, HIGH);
 
 }
 
 void loop(){
-  Serial.println("HELLO");
+  /* Serial.println("HELLO"); */
   char key = keypad.getKey();
-  if (key != NO_KEY){
+  bool receiver = (digitalRead(BUTTON_PIN) == HIGH);
+  Serial.println(receiver);
+
+  if (key != NO_KEY){// && digitalRead(BUTTON_PIN) == HIGH){
     Serial.println(key);
   }
 
